edac5a1fb6d3e40af56ddcfa776b76288c5b99a9
David Puziol Prata <davidpuziol@gmail.com>
2023-08-20 07:51:04 -0300

    first commit to test

--- outputs.tf
+++ outputs.tf
@@ -15,72 +15,72 @@ output "aws_vpc_cidr_block" {
 
 output "aws_azs" {
   description = "list of azs of the network"
-  value       = var.cloud == "aws" ? toset(local.azs) : null
+  value       = var.cloud == "aws" ? local.azs : null
 }
 
 output "aws_intra_subnet_arns" {
   description = "arn of the intra subnets"
-  value       = var.cloud == "aws" ? toset(module.vpc.intra_subnet_arns) : null
+  value       = var.cloud == "aws" ? module.vpc.intra_subnet_arns : null
 }
 
 output "aws_intra_subnet_ids" {
   description = "IDs of the intra subnets"
-  value       = var.cloud == "aws" ? toset(module.vpc.intra_subnets) : null
+  value       = var.cloud == "aws" ? module.vpc.intra_subnets : null
 }
 
 output "aws_intra_subnets_cidr_blocks" {
   description = "List of cidr_blocks of intra subnets"
-  value       = var.cloud == "aws" ? toset(module.vpc.intra_subnets_cidr_blocks) : null
+  value       = var.cloud == "aws" ? module.vpc.intra_subnets_cidr_blocks : null
 }
 
 output "aws_intra_route_table_ids" {
   description = "ids of the route tables from intra subnet"
-  value       = var.cloud == "aws" ? toset(module.vpc.intra_route_table_ids) : null
+  value       = var.cloud == "aws" ? module.vpc.intra_route_table_ids : null
 }
 
 output "aws_public_subnet_arns" {
   description = "arn of the intra subnets"
-  value       = var.cloud == "aws" ? toset(module.vpc.public_subnet_arns) : null
+  value       = var.cloud == "aws" ? module.vpc.public_subnet_arns : null
 }
 
 output "aws_public_subnet_ids" {
   description = "IDs of the intra subnets"
-  value       = var.cloud == "aws" ? toset(module.vpc.public_subnets) : null
+  value       = var.cloud == "aws" ? module.vpc.public_subnets : null
 }
 
 output "aws_public_subnets_cidr_blocks" {
   description = "List of cidr_blocks of public subnets"
-  value       = var.cloud == "aws" ? toset(module.vpc.public_subnets_cidr_blocks) : null
+  value       = var.cloud == "aws" ? module.vpc.public_subnets_cidr_blocks : null
 }
 
 output "aws_public_route_table_ids" {
   description = "ids of the route tables from public subnet"
-  value       = var.cloud == "aws" ? toset(module.vpc.public_route_table_ids) : null
+  value       = var.cloud == "aws" ? module.vpc.public_route_table_ids : null
 }
 
 output "aws_private_subnet_arns" {
   description = "arn of the intra subnets"
-  value       = var.cloud == "aws" ? toset(module.vpc.private_subnet_arns) : null
+  value       = var.cloud == "aws" ? module.vpc.private_subnet_arns : null
 }
 
 output "aws_private_subnet_ids" {
   description = "IDs of the intra subnets"
-  value       = var.cloud == "aws" ? toset(module.vpc.private_subnets) : null
+  value       = var.cloud == "aws" ? module.vpc.private_subnets : null
 }
 
 output "aws_private_subnets_cidr_blocks" {
   description = "List of cidr_blocks of private subnets"
-  value       = var.cloud == "aws" ? toset(module.vpc.private_subnets_cidr_blocks) : null
+  value       = var.cloud == "aws" ? module.vpc.private_subnets_cidr_blocks : null
 }
 
 output "aws_private_route_table_ids" {
   description = "ids of the route tables from private subnet"
-  value       = var.cloud == "aws" ? toset(module.vpc.private_route_table_ids) : null
+  value       = var.cloud == "aws" ? module.vpc.private_route_table_ids : null
 }
 
 output "aws_nat_public_ips" {
   description = "List of public Elastic IPs created for AWS NAT Gateway"
-  value       = var.cloud == "aws" ? toset(module.vpc.nat_public_ips) : null
+  value       = var.cloud == "aws" ? module.vpc.nat_public_ips : null
 }
 
 
